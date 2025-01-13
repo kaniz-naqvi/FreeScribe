9f7c521994e842a149d592c0d373a0822d53579c
Mehak <mehak313naqvi@gmail.com>
2025-01-13 19:56:11 +0500

    Added setAudion functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from "react";
 import "./App.css";
 import HomePage from "./components/HomePage";
 import Header from "./components/Header";
+import FileDisplay from "./components/FileDisplay";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [file, setFile] = useState(null);
+  const [audio, setAudio] = useState(null);
+  const isAudioAvaliable = file || audio;
+  function handleAudioReset() {
+    setAudio(null);
+    setFile(null);
+  }
   return (
     <div className="flex flex-col">
       <section className="min-h-screen flex flex-col">
         <Header />
-        <HomePage />
+        {isAudioAvaliable ? (
+          <FileDisplay
+            handleAudioReset={handleAudioReset}
+            setAudio={setAudio}
+            file={file}
+          />
+        ) : (
+          <HomePage setAudio={setAudio} setFile={setFile} />
+        )}
       </section>
     </div>
   );
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
-const HomePage = () => {
+const HomePage = ({ setFile, setAudio }) => {
+  const [recordingStatus, setRecordingStatus] = useState("inactive");
+  const [audioChunks, setAudioChunks] = useState([]);
+  const [duration, setDuration] = useState(0);
+  const mediaRecorder = useRef(null);
+  const mimeType = "audio/webm";
+  async function startRecording() {
+    let tempSteam;
+    console.log("Start recording");
+    try {
+      const steamData = navigator.mediaDevices.getUserMedia({
+        audio: true,
+        video: false,
+      });
+      tempSteam = steamData;
+    } catch (error) {
+      console.log(error.message);
+    }
+    const media = new MediaRecorder(tempSteam, { type: mimeType });
+    mediaRecorder.current = media;
+    mediaRecorder.current.start();
+    let localAudioChunks = [];
+    mediaRecorder.current.ondataavailable = (e) => {
+      if (typeof e.data === "undefined") {
+        return;
+      }
+      if (e.dat.size === 0) {
+        return;
+      }
+      localAudioChunks.push(e.data);
+      setAudio(localAudioChunks);
+    };
+  }
   return (
     <main className="flex-1 p-4 flex flex-col text-center justify-center">
       <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
@@ -17,9 +49,17 @@ const HomePage = () => {
       </button>
       <p className="text-base">
         Or&#160;
-        <label className="text-blue cursor-pointer hover:text-blue-500 duration-200">
+        <label className="text-blue-500 cursor-pointer hover:text-blue-700 duration-200">
           Upload&#160;
-          <input type="file" className="hidden" accept=".mp3, .wave" />
+          <input
+            onChange={(e) => {
+              const tempFile = e.target.files[0];
+              setFile(tempFile);
+            }}
+            type="file"
+            className="hidden"
+            accept=".mp3, .wave"
+          />
         </label>
         a mp3 file
       </p>
