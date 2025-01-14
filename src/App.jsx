08117a7b6d242d5b4ecce7bc244f30c5a75eb6f6
Mehak <mehak313naqvi@gmail.com>
2025-01-14 16:28:22 +0500

    added comments explaining the essential parts

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import { useState } from "react";
-import "./App.css";
-import HomePage from "./components/HomePage";
-import Header from "./components/Header";
-import FileDisplay from "./components/FileDisplay";
+import { useState } from "react"; // Importing useState hook
+import "./App.css"; // Importing CSS
+import HomePage from "./components/HomePage"; // Importing HomePage component
+import Header from "./components/Header"; // Importing Header component
+import FileDisplay from "./components/FileDisplay"; // Importing FileDisplay component
 
 function App() {
-  const [file, setFile] = useState(null);
-  const [audio, setAudio] = useState(null);
-  const isAudioAvaliable = file || audio;
+  const [file, setFile] = useState(null); // State for file
+  const [audio, setAudio] = useState(null); // State for audio
+  const isAudioAvaliable = file || audio; // Check if audio or file is available
+
+  // Reset audio and file states
   function handleAudioReset() {
     setAudio(null);
     setFile(null);
   }
+
   return (
     <div className="flex flex-col">
       <section className="min-h-screen flex flex-col">
-        <Header />
+        <Header /> // Header component
         {isAudioAvaliable ? (
           <FileDisplay
             handleAudioReset={handleAudioReset}
@@ -30,4 +33,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; // Exporting App component
